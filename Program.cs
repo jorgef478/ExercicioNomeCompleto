ac906d22973bb464816bca6cc9fdc93e44c21a4d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-09-01 21:40:46 -0300

    Programa finalizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Olá, bem vindo!");
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.WriteLine("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine()!;
+
+string nomeCompleto = nome + " " + sobrenome;
+string nomeCatalogo = sobrenome + " " + nome;
+
+Console.Write("Nome completo: ");
+Console.WriteLine (nomeCompleto);
+Console.Write("Nome de catálogo: ");
+Console.WriteLine (nomeCatalogo);
+
+
+
+
+
+
+
